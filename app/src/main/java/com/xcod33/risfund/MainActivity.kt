575842ae332691b842cdaf8f8ab3df9342b3b040
sakanusiw <saka.interisti@gmail.com>
2022-12-22 15:43:05 +0700

    shapes, kotlin main activity splash screen and login layouting

--- app/src/main/java/com/xcod33/risfund/MainActivity.kt
+++ app/src/main/java/com/xcod33/risfund/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.xcod33.risfund
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Set a delay for the splash screen
+        val splashDelay: Long = 3000 // 3 seconds
+
+        Handler().postDelayed({
+            // Launch the main activity
+            startActivity(Intent(this, LoginActivity::class.java))
+            // Close the splash screen activity
+            finish()
+        }, splashDelay)
     }
 }
