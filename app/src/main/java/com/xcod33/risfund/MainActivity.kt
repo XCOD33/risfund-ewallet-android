a3e30805812d2db5b97611a1fff85f660c3b568d
Ivanbhagaskara2608 <ivanbhagas2003@gmail.com>
2022-12-25 18:48:25 +0700

    add customcardpurchase styles.xml

--- app/src/main/java/com/xcod33/risfund/MainActivity.kt
+++ app/src/main/java/com/xcod33/risfund/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
 
         Handler().postDelayed({
             // Launch the main activity
-            startActivity(Intent(this, Register1Activity::class.java))
+            startActivity(Intent(this, PulsaActivity::class.java))
             // Close the splash screen activity
             finish()
         }, splashDelay)
