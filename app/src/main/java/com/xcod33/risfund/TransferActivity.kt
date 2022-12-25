c4eaa997012eb91b716dd7c7fe9738dc82740448
FajarWnugra <fajarwn474@gmail.com>
2022-12-25 17:26:41 +0700

    add transfer activity

--- app/src/main/java/com/xcod33/risfund/TransferActivity.kt
+++ app/src/main/java/com/xcod33/risfund/TransferActivity.kt
@@ -3,7 +3,7 @@ package com.xcod33.risfund
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Transfer : AppCompatActivity() {
+class TransferActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transfer)
