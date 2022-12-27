c8149c177b7b430d43df0e3f840a59ac4b940d45
sakanusiw <saka.interisti@gmail.com>
2022-12-27 20:24:37 +0700

    adding 7 more Activities

---
+++ app/src/main/java/com/xcod33/risfund/MyQrActivity.kt
@@ -0,0 +1,11 @@
+package com.xcod33.risfund
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyQrActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_qr)
+    }
+}
---
+++ app/src/main/java/com/xcod33/risfund/PaymentSuccessActivity.kt
@@ -0,0 +1,11 @@
+package com.xcod33.risfund
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PaymentSuccessActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_success)
+    }
+}
