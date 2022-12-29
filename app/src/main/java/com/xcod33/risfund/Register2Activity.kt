85c4aeb95c96f7cb82fd7e044ca03bc0e2569480
Ivanbhagaskara2608 <ivanbhagas2003@gmail.com>
2022-12-29 21:56:48 +0700

    edit Register2Activity.kt, edit webview

--- app/src/main/java/com/xcod33/risfund/Register2Activity.kt
+++ app/src/main/java/com/xcod33/risfund/Register2Activity.kt
@@ -2,6 +2,7 @@ package com.xcod33.risfund
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import com.google.android.material.textfield.TextInputLayout
@@ -10,7 +11,11 @@ class Register2Activity : AppCompatActivity() {
 
     private lateinit var usernameRegisterInputLayout: TextInputLayout
     private lateinit var usernameRegisterEditText: EditText
-
+    private lateinit var passwordRegisterInputLayout: TextInputLayout
+    private lateinit var passwordRegisterEditText: EditText
+    private lateinit var rePasswordRegisterInputLayout: TextInputLayout
+    private lateinit var rePasswordRegisterEditText: EditText
+    private lateinit var daftarRegisterButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +23,23 @@ class Register2Activity : AppCompatActivity() {
 
 
         val bundle = intent.extras
+        val name = bundle?.getString("name")
+        val phoneNumber = bundle?.getString("phoneNumber")
         val gender = bundle?.getString("gender")
 
+        usernameRegisterInputLayout = findViewById(R.id.usernameRegisterInputLayout)
+        usernameRegisterEditText = findViewById(R.id.usernameRegisterEditText)
+        passwordRegisterInputLayout = findViewById(R.id.passwordRegisterInputLayout)
+        passwordRegisterEditText = findViewById(R.id.passwordRegisterEditText)
+        rePasswordRegisterInputLayout = findViewById(R.id.rePasswordRegisterInputLayout)
+        rePasswordRegisterEditText = findViewById(R.id.rePasswordRegisterEditText)
+        daftarRegisterButton = findViewById(R.id.daftarRegisterButton)
+
+        daftarRegisterButton.setOnClickListener {
+            if (usernameRegisterEditText.text.isEmpty()) {
+
+            }
+        }
 
     }
 }
