7feb128245591f90b739d75df69e074659f8ee9a
FajarWnugra <fajarwn474@gmail.com>
2022-12-29 20:37:17 +0700

    add rectangle66.xml & edit activity_my_qe.xml

--- app/src/main/java/com/xcod33/risfund/MainActivity.kt
+++ app/src/main/java/com/xcod33/risfund/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
 
         Handler().postDelayed({
             // Launch the main activity
-            startActivity(Intent(this, LoginActivity::class.java))
+            startActivity(Intent(this, MyQrActivity::class.java))
             // Close the splash screen activity
             finish()
         }, splashDelay)
