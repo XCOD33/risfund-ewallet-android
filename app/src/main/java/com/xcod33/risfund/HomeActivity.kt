a3952014d49815743df41c6464616d5c94016a42
sakanusiw <saka.interisti@gmail.com>
2022-12-29 20:47:33 +0700

    fixing HomeActivity

--- app/src/main/java/com/xcod33/risfund/HomeActivity.kt
+++ app/src/main/java/com/xcod33/risfund/HomeActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
 
@@ -18,6 +19,7 @@ class HomeActivity : AppCompatActivity() {
     private lateinit var wifiButton: ImageButton
     private lateinit var mtixButton: ImageButton
     private lateinit var bpjsButton: ImageButton
+    private lateinit var bottomNav: com.google.android.material.bottomnavigation.BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +36,8 @@ class HomeActivity : AppCompatActivity() {
         wifiButton = findViewById(R.id.wifiButton)
         mtixButton = findViewById(R.id.mtixButton)
         bpjsButton = findViewById(R.id.bpjsButton)
+        bottomNav = findViewById(R.id.bottomNav)
+        transferButton = findViewById(R.id.transferButton)
 
         topUpButton.setOnClickListener {
             var intent = Intent(this, TopUpActivity::class.java)
@@ -88,6 +92,41 @@ class HomeActivity : AppCompatActivity() {
         bpjsButton.setOnClickListener {
             var intent = Intent(this, BpjsActivity::class.java)
             startActivity(intent)
+        }
+
+        transferButton.setOnClickListener {
+            var intent = Intent(this, TransferActivity::class.java)
+            startActivity(intent)
+        }
+
+        bottomNav.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.homeNavigation -> {
+                    val intent = Intent(this, HomeActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.qrNavigation-> {
+                    val intent = Intent(this, MyQrActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.historyNavigation -> {
+                    val intent = Intent(this, HistoryActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.settingNavigation -> {
+                    val intent = Intent(this, SettingsActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                else -> false
+            }
         }
     }
 }
