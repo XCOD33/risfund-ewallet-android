6f71b31d6a2c5fd2132584c79c563d404054a998
FajarWnugra <fajarwn474@gmail.com>
2022-12-30 09:18:34 +0700

    add back transfer

--- app/src/main/java/com/xcod33/risfund/TransferActivity.kt
+++ app/src/main/java/com/xcod33/risfund/TransferActivity.kt
@@ -1,11 +1,21 @@
 package com.xcod33.risfund
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class TransferActivity : AppCompatActivity() {
+    private lateinit var backTransfer: ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transfer)
+
+        backTransfer = findViewById(R.id.backTransfer)
+
+        backTransfer.setOnClickListener {
+            var intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
