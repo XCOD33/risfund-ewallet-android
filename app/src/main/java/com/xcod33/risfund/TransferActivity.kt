ba9c3139549fc8230079fd66ec5414c79cc05833
FajarWnugra <fajarwn474@gmail.com>
2022-12-30 10:10:35 +0700

    edit transferActivity.kt

--- app/src/main/java/com/xcod33/risfund/TransferActivity.kt
+++ app/src/main/java/com/xcod33/risfund/TransferActivity.kt
@@ -3,18 +3,28 @@ package com.xcod33.risfund
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 
 class TransferActivity : AppCompatActivity() {
+
     private lateinit var backTransfer: ImageButton
+    private lateinit var transferButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transfer)
 
         backTransfer = findViewById(R.id.backTransfer)
+        transferButton = findViewById(R.id.TransferButton)
 
         backTransfer.setOnClickListener {
             var intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        transferButton.setOnClickListener{
+            var intent = Intent(this, TransferConfirmationActivity::class.java)
             startActivity(intent)
         }
     }
