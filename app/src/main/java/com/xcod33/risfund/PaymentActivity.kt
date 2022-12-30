85bccd955d1ebca888c402b3a1726eca853d7bdd
Ivanbhagaskara2608 <ivanbhagas2003@gmail.com>
2022-12-30 15:37:14 +0700

    edit PaymentActivity.kt

--- app/src/main/java/com/xcod33/risfund/PaymentActivity.kt
+++ app/src/main/java/com/xcod33/risfund/PaymentActivity.kt
@@ -1,25 +1,37 @@
 package com.xcod33.risfund
 
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.budiyev.android.codescanner.AutoFocusMode
 import com.budiyev.android.codescanner.CodeScanner
 import com.budiyev.android.codescanner.CodeScannerView
 import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
 import com.budiyev.android.codescanner.ScanMode
 import java.util.jar.Manifest
 
 class PaymentActivity : AppCompatActivity() {
 
     private lateinit var codescanner: CodeScanner
+    private lateinit var backPayment: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment)
+
+        backPayment = findViewById(R.id.backPayment)
+
+        backPayment.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
 
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) ==
                 PackageManager.PERMISSION_DENIED){
@@ -43,8 +55,48 @@ class PaymentActivity : AppCompatActivity() {
 
         codescanner.decodeCallback = DecodeCallback {
             runOnUiThread {
-
+                Toast.makeText(this, "Result = ${it.text}", Toast.LENGTH_SHORT).show()
             }
         }
+
+        codescanner.errorCallback = ErrorCallback {
+            runOnUiThread {
+                Toast.makeText(this, "Kamera initilization error = ${it.message}", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        scannerView.setOnClickListener {
+            codescanner.startPreview()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 123){
+            if (grantResults[0] == PackageManager.PERMISSION_DENIED){
+                Toast.makeText(this, "Camera permission granted", Toast.LENGTH_SHORT).show()
+                startScanning()
+            } else{
+                Toast.makeText(this, "Camera permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (::codescanner.isInitialized){
+            codescanner?.startPreview()
+        }
+    }
+
+    override fun onPause() {
+        if (::codescanner.isInitialized){
+            codescanner?.releaseResources()
+        }
+        super.onPause()
     }
 }
