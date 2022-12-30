9eaf50618d04e5f06e11bd17a39de534b5724b9e
FajarWnugra <fajarwn474@gmail.com>
2022-12-30 15:54:19 +0700

    edit transferSuccessActivity.kt

--- app/src/main/java/com/xcod33/risfund/TransferSuccessActivity.kt
+++ app/src/main/java/com/xcod33/risfund/TransferSuccessActivity.kt
@@ -1,11 +1,32 @@
 package com.xcod33.risfund
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class TransferSuccessActivity : AppCompatActivity() {
+
+    private lateinit var backTransferSukses: ImageButton
+    private lateinit var transferBerhasilButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transfer_success)
+
+        backTransferSukses = findViewById(R.id.backTransferSukses)
+
+        backTransferSukses.setOnClickListener{
+            var intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        transferBerhasilButton = findViewById(R.id.transferBerhasilButton)
+
+        transferBerhasilButton.setOnClickListener{
+            var intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
