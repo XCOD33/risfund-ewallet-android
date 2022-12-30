d603e3805bccea31eb88a17a60eb13d7c22fc60b
Ivanbhagaskara2608 <ivanbhagas2003@gmail.com>
2022-12-30 14:59:59 +0700

    add myQRactivity

--- app/src/main/java/com/xcod33/risfund/MyQrActivity.kt
+++ app/src/main/java/com/xcod33/risfund/MyQrActivity.kt
@@ -1,11 +1,42 @@
 package com.xcod33.risfund
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.WriterException
+import com.google.zxing.qrcode.QRCodeWriter
 
 class MyQrActivity : AppCompatActivity() {
+
+    private lateinit var qrImageView: ImageView
+    private lateinit var bottomNav: com.google.android.material.bottomnavigation.BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_qr)
+
+        qrImageView = findViewById(R.id.qrImageView)
+        val data = "ivan"
+        val writer = QRCodeWriter()
+        try {
+
+            val bitMatrix = writer.encode(data, BarcodeFormat.QR_CODE, 612, 612)
+            val width = bitMatrix.width
+            val heigth = bitMatrix.height
+            val bmp = Bitmap.createBitmap(width, heigth, Bitmap.Config.RGB_565)
+            for (x in 0 until width){
+                for (y in 0 until heigth) {
+                    bmp.setPixel(x, y, if (bitMatrix[x, y]) Color.BLACK else Color.WHITE)
+                }
+            }
+            qrImageView.setImageBitmap(bmp)
+        } catch (e: WriterException){
+            e.printStackTrace()
+        }
+
+
     }
 }
--- app/src/main/java/com/xcod33/risfund/PaymentActivity.kt
+++ app/src/main/java/com/xcod33/risfund/PaymentActivity.kt
@@ -1,11 +1,50 @@
 package com.xcod33.risfund
 
+import android.content.Context
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ScanMode
+import java.util.jar.Manifest
 
 class PaymentActivity : AppCompatActivity() {
+
+    private lateinit var codescanner: CodeScanner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment)
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) ==
+                PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), 123)
+        } else {
+            startScanning()
+        }
+
+    }
+
+    private fun startScanning() {
+        val scannerView: CodeScannerView = findViewById(R.id.scanner_view)
+        codescanner = CodeScanner(this, scannerView)
+        codescanner.camera = CodeScanner.CAMERA_BACK
+        codescanner.formats = CodeScanner.ALL_FORMATS
+
+        codescanner.autoFocusMode = AutoFocusMode.SAFE
+        codescanner.scanMode = ScanMode.SINGLE
+        codescanner.isAutoFocusEnabled = true
+        codescanner.isFlashEnabled = false
+
+        codescanner.decodeCallback = DecodeCallback {
+            runOnUiThread {
+
+            }
+        }
     }
 }
