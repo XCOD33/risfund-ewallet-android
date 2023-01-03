2a17e3a25606abfadc89b3e177651629f68e7583
sakanusiw <saka.interisti@gmail.com>
2023-01-03 11:55:57 +0700

    edit HistoryActivity, card_view_riwayat, activity_history, add CustomAdapterRiwayat, ItemsViewModelRiwayat

--- app/src/main/java/com/xcod33/risfund/HistoryActivity.kt
+++ app/src/main/java/com/xcod33/risfund/HistoryActivity.kt
@@ -3,17 +3,20 @@ package com.xcod33.risfund
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class HistoryActivity : AppCompatActivity() {
 
     private lateinit var bottomNav: com.google.android.material.bottomnavigation.BottomNavigationView
+    private lateinit var riwayatRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
 
         bottomNav = findViewById(R.id.bottomNav)
-        bottomNav.setSelectedItemId(R.id.historyNavigation);
+        bottomNav.setSelectedItemId(R.id.historyNavigation)
 
         bottomNav.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
@@ -42,5 +45,14 @@ class HistoryActivity : AppCompatActivity() {
                 else -> false
             }
         }
+
+        riwayatRecyclerView.layoutManager = LinearLayoutManager(this)
+        val data = ArrayList<ItemsViewModelRiwayat>()
+        for(i in 1..7) {
+            data.add(ItemsViewModelRiwayat(i, i, i, i))
+        }
+
+        val adapter = CustomAdapterRiwayat(data)
+        riwayatRecyclerView.adapter = adapter
     }
 }
---
+++ app/src/main/java/com/xcod33/risfund/ItemsViewModelRiwayat.kt
@@ -0,0 +1,39 @@
+package com.xcod33.risfund
+
+data class ItemsViewModelRiwayat (val image: Int, val title: Int, val date: Int, val price: Int) {
+    val getImage = when(image) {
+        1 -> R.drawable.menupulsa
+        2 -> R.drawable.menupaketdata
+        3 -> R.drawable.menubpjs
+        4 -> R.drawable.menuwifi
+        5 -> R.drawable.menupdam
+        else -> "NULL"
+    }
+
+    val getTitle = when(title) {
+        1 -> "Pulsa"
+        2 -> "Paket Data"
+        3 -> "BPJS"
+        4 -> "Wifi"
+        5 -> "PDAM"
+        else -> "NULL"
+    }
+
+    val getDate = when(date) {
+        1 -> "12 Desember 2022 - 09:00"
+        2 -> "10 Desember 2022 - 10:45"
+        3 -> "10 Desember 2022 - 07:50"
+        4 -> "9 Desember 2022 - 11:00"
+        5 -> "7 Desember 2022 - 22.50"
+        else -> "NULL"
+    }
+
+    val getPrice = when(price) {
+        1 -> "Rp40,000"
+        2 -> "Rp50,000"
+        3 -> "Rp60,000"
+        4 -> "Rp75,000"
+        5 -> "Rp90,000"
+        else -> "NULL"
+    }
+}
