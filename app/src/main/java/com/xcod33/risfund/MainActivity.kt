4cd7e451affd6dc960fbdb4feda1dd6b60c9c6d6
FajarWnugra <fajarwn474@gmail.com>
2023-01-05 14:42:14 +0700

    edit xmls from rekative to constraint layout

--- app/src/main/java/com/xcod33/risfund/MainActivity.kt
+++ app/src/main/java/com/xcod33/risfund/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
 
         Handler().postDelayed({
             // Launch the main activity
-            startActivity(Intent(this, HomeActivity2::class.java))
+            startActivity(Intent(this, PlnPascabayarActivity::class.java))
             // Close the splash screen activity
             finish()
         }, splashDelay)
