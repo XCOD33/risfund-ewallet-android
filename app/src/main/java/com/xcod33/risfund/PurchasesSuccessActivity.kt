2a3ac8399b96dff9e4613d79aac474860ef56f7e
XCOD33 <denistraju155@gmail.com>
2023-01-08 19:01:32 +0700

    done with pulsa

--- app/src/main/java/com/xcod33/risfund/PurchasesSuccessActivity.kt
+++ app/src/main/java/com/xcod33/risfund/PurchasesSuccessActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import com.xcod33.risfund.data.GetUserResponse
+import kotlinx.android.synthetic.main.activity_purchases_success.*
 
 class PurchasesSuccessActivity : AppCompatActivity() {
 
@@ -15,17 +17,27 @@ class PurchasesSuccessActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_purchases_success)
 
+        val user = intent.getParcelableExtra<GetUserResponse>("dataUser")
+
         backPembayaranSuccess = findViewById(R.id.backPembayaranSukses)
         pembayaranBerhasilButton = findViewById(R.id.pembayaranBerhasilButton)
 
         backPembayaranSuccess.setOnClickListener {
-            var intent = Intent(this, HomeActivity::class.java)
+            var intent = Intent(this, HomeActivity2::class.java)
+            intent.putExtra("dataUser", user)
             startActivity(intent)
         }
 
         pembayaranBerhasilButton.setOnClickListener {
-            var intent = Intent(this, HomeActivity::class.java)
+            var intent = Intent(this, HomeActivity2::class.java)
+            intent.putExtra("dataUser", user)
             startActivity(intent)
+        }
+
+        val intent = intent
+        if(intent.getStringExtra("type") == "pulsa") {
+            titleSuccessTextView.text = intent.getStringExtra("purchaseType")
+            PembayaranAmountTextView.text = "- Rp " + intent.getStringExtra("amount")
         }
     }
 }
