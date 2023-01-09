1be4d1d91dd9070f688bfa6230e703f7e8fa0331
Ivanbhagaskara2608 <ivanbhagas2003@gmail.com>
2023-01-09 21:43:00 +0700

    add pdam, wifi, bpjs confirmation activity

--- app/src/main/java/com/xcod33/risfund/PdamActivity.kt
+++ app/src/main/java/com/xcod33/risfund/PdamActivity.kt
@@ -5,21 +5,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 import com.xcod33.risfund.data.GetUserResponse
+import kotlinx.android.synthetic.main.activity_pdam.*
 
 class PdamActivity : AppCompatActivity() {
-
-    private lateinit var backPdam: ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pdam)
 
         val user = intent.getParcelableExtra<GetUserResponse>("dataUser")
-        backPdam = findViewById(R.id.backPdam)
 
         backPdam.setOnClickListener {
             var intent = Intent(this, HomeActivity2::class.java)
             intent.putExtra("dataUser", user)
             startActivity(intent)
         }
+
+        pdamButton.setOnClickListener {
+            createTransaction(user)
+        }
+    }
+
+    private fun createTransaction(user: GetUserResponse?) {
+
     }
 }
